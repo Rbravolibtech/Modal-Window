fce78cfd66974c4db8910f291e26b504451ad6d1
Rbravolibtech <122310916+Rbravolibtech@users.noreply.github.com>
2023-07-10 16:34:08 -0600

    added clickable modal and to close

--- script.js
+++ script.js
@@ -4,7 +4,22 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnOpenModal = document.querySelectorAll('.show-modal');
-console.log(btnOpenModal);
 
-for (let i = 0; i < btnOpenModal.length; i++);
-console.log(btnOpenModal[i].textContent);
+// This code make so you can click the modal 1 or modal 2 or modal 3
+
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+// This code makes it so you can close the modal on X
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+for (let i = 0; i < btnOpenModal.length; i++)
+  btnOpenModal[i].addEventListener('click', openModal);
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
